4ccf178da17b119b28037f09eab83bab5a608574
tommy-carlos williams <tommy@devgeeks.org>
2016-02-10 16:58:17 +1100

    Themes (Material and iOS) as well as custom colours
    index.html
    
    1. \<body class="theme-green"\> // Change to a "green" theme to match Spotify a bit
    2. Remove CSS link tags in prep for dynamic theming below
    3. OCD 2-space indents
    
    js/my-app.js
    
    1. All the JS code from [Maintain both iOS and Material Themes in single app](http://www.idangero.us/framework7/tutorials/maintain-both-ios-and-material-themes-in-single-app.html)
    	* This has caused a bug with the title no longer being centered in iOS :/
    	* Issue raised: https://github.com/nolimits4web/Framework7/issues/837
    	* Temp fix: `setTimeout(function() { myApp.sizeNavbars('.view-main'); }, 10);`
    	* Another minor bug, the `.content-block` sits UNDER the navbar in the Material theme
    	* Temp fix: `.material .page .page-content > .content-block { margin-top: 78px; }`
    2. Additionally add a class to \<body\> of either 'material' or 'ios'
    2. OCD 2-space indents
    
    www/css/styles.css
    
    1. OCD 2-space indents

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,21 +1,61 @@
-// Initialize app
-var myApp = new Framework7();
+// Code for platform detection
+var isAndroid = Framework7.prototype.device.android === true;
+var isIos = Framework7.prototype.device.ios === true;
 
+Template7.global = {
+  android: isAndroid,
+  ios: isIos
+};
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
+// Add platform styles
+if (isAndroid) {
+  $$('head').append(
+    '<link rel="stylesheet" href="lib/framework7/css/framework7.material.min.css">' +
+    '<link rel="stylesheet" href="lib/framework7/css/framework7.material.colors.min.css">' +
+    '<link rel="stylesheet" href="css/styles.css">'
+  );
+  $$('body').addClass('material');
+}
+else {
+  $$('head').append(
+    '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.min.css">' +
+    '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.colors.min.css">' +
+    '<link rel="stylesheet" href="css/styles.css">'
+  );
+  $$('body').addClass('android');
+}
+
+if (isAndroid) {
+  // Change class
+  $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
+  // And move Navbar into Page
+  $$('.view .navbar').prependTo('.view .page');
+}
+
+// Initialize app
+var myApp = new Framework7({
+  material: isAndroid ? true : false,
+  template7Pages: true,
+});
+
 // Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
 });
+
+setTimeout(function() {
+  myApp.sizeNavbars('.view-main');
+}, 10);
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+  console.log('Device is ready!');
 });
 
-//myApp.onPageInit('about', function (page) {
-//  // Handle about page
-//});
+myApp.onPageInit('index', function (page) {
+  // ...
+});
