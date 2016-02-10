0f178314e4e1e33aa6f18f4b974ce28aeb91ceb2
tommy-carlos williams <tommy@devgeeks.org>
2016-02-11 07:35:37 +1100

    A fix for Framework7 issue 837
    index.html
    
    1. `<script type="text/javascript" src="js/init-styles.js"></script>` added before `my-app.js`
      * Uses code from https://github.com/nolimits4web/Framework7/issues/837#issuecomment-182348815 but in a standalone file
    
    js/init-styles.js added
    
    js/my-app.js
    
    1. Removed code for adding stylesheets to the head
    2. Removed the fragile `setTimeout` added to fix the navbar title issue

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -10,24 +10,6 @@ Template7.global = {
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-// Add platform styles
-if (isAndroid) {
-  $$('head').append(
-    '<link rel="stylesheet" href="lib/framework7/css/framework7.material.min.css">' +
-    '<link rel="stylesheet" href="lib/framework7/css/framework7.material.colors.min.css">' +
-    '<link rel="stylesheet" href="css/styles.css">'
-  );
-  $$('body').addClass('material');
-}
-else {
-  $$('head').append(
-    '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.min.css">' +
-    '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.colors.min.css">' +
-    '<link rel="stylesheet" href="css/styles.css">'
-  );
-  $$('body').addClass('android');
-}
-
 if (isAndroid) {
   // Change class
   $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
@@ -46,10 +28,6 @@ var mainView = myApp.addView('.view-main', {
   // Because we want to use dynamic navbar, we need to enable it for this view:
   dynamicNavbar: true
 });
-
-setTimeout(function() {
-  myApp.sizeNavbars('.view-main');
-}, 10);
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
