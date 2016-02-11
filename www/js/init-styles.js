ddd9a2b2452c3241abbb6a1020b48889cdf5ebaa
tommy-carlos williams <tommy@devgeeks.org>
2016-02-11 19:33:08 +1100

    Add search form and allow the developer app
    index.html
    
    1. Commented out CSP tag as it blocks the use of the mobile developer app refresh and live reload.
    	* I am commenting it out during development. Will be a good thing to note in the tutorials
    2. Changed the title to "Media App"
    3. Added search form
    	* Search text field
    	* Explicit toggle
    	* Checkboxes to include/exclude tracks, song, or artist from search
    	* Search submit buton
    
    *Note: the changes below were also made to default to using Material Design where we are not in iOS. This means that if another platform is added, it will at least get the more generic Material theme*
    
    js/init-styles.js
    
    1. Changed `if` to test for iOS leaving the `else` to deliver Material for everything else
    2. Added a class to the \<body\> tag to denote 'material' or 'ios' theme so that custom styles in `css/styles.css` could target one or the other.
    
    js/my-app.js
    
    1. Again, flipped the default case from iOS to Material
    2. Added swipe-to-close on left side panel
    
    css/styles.css
    
    1. Added some margin-top to content-block and content-block-title under Material. Seems to be a bug possibly related to moving the navbar to support Material and iOS.
    2. Added a larger line-height to the label-checkbox item-title. It didn't look lined up enough (again, in Material)

--- www/js/init-styles.js
+++ www/js/init-styles.js
@@ -1,15 +1,20 @@
 (function () {
-  if (Framework7.prototype.device.android) {
+  if (Framework7.prototype.device.ios) {
     Dom7('head').append(
-        '<link rel="stylesheet" href="lib/framework7/css/framework7.material.min.css">' +
-        '<link rel="stylesheet" href="lib/framework7/css/framework7.material.colors.min.css">'
-        );
+      '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.min.css">' +
+      '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.colors.min.css">' +
+      '<link rel="stylesheet" href="css/styles.css">'
+    );
+    Dom7('body').addClass('ios');
   }
   else {
+    // Default to Material
     Dom7('head').append(
-        '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.min.css">' +
-        '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.colors.min.css">'
-        );
+      '<link rel="stylesheet" href="lib/framework7/css/framework7.material.min.css">' +
+      '<link rel="stylesheet" href="lib/framework7/css/framework7.material.colors.min.css">' +
+      '<link rel="stylesheet" href="css/styles.css">'
+    );
+    Dom7('body').addClass('material');
   }
 })();
 
